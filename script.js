15c093497f0de93cddb569b79551346e59a5cfff
jakradum <145890080+jakradum@users.noreply.github.com>
2023-10-22 21:23:44 +0530

    delete class added to x button

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-// checklist items related script below:
-const todo= document.getElementById("todo-list");
-const buttonClick=document.getElementById("todo-button");
-const input1=document.getElementById("userinput");
-const listArray=[];
-
+// insert date function copied from ChatGPT and edited slightly
+const today = new Date();
+const options = { day: 'numeric' ,month: 'short'};
+const formattedDate = today.toLocaleDateString('en', options);
+document.getElementById("date-heading").innerHTML="Tasks on: "+formattedDate;
 // if list is empty error msg
 function taskListEmpty() {
 	if(listArray.length===0) {document.getElementById("errormsg").innerHTML="No tasks"}
@@ -12,6 +10,12 @@ function taskListEmpty() {
 	else{document.getElementById("errormsg").innerHTML=listArray.length+" tasks"};
 }
 
+// checklist items related to the script below:
+const todo= document.getElementById("todo-list");
+const buttonClick=document.getElementById("todo-button");
+const input1=document.getElementById("userinput");
+const xButton=document.querySelectorAll("span");
+const listArray=[];
 // checklist creation function below:
 function createListElement () {
 	if(input1.value.length>0){
@@ -21,48 +25,39 @@ function createListElement () {
 	const closeButton= document.createElement("span"); // declare a new constant to create a span element, will be used later
 	const task=document.querySelectorAll("li");
 	addCheckbox.type="checkbox";
+	list.classList.add("task") 
+	// event listener for checkbox starts below
 	addCheckbox.addEventListener("change",function() {
 	if(addCheckbox.checked) {
-		// list.classList.add("checkbox")
 		list.classList.add("done")
 	} else {
-		list.classList.add("new")
 		list.classList.remove("done");
 	}}
 	);
-	// add event listener called "change" and declare a function where, using 'classList' you can add or remove classes.
+	closeButton.innerHTML="✕"; // span element being added to the html here
 	list.appendChild(addCheckbox);
 	list.appendChild(listCreateNode);
-	
-	closeButton.innerHTML=" ✕"; // span element being added to the html here
+	list.appendChild(closeButton);
 	list.appendChild(closeButton);
 	todo.appendChild(list);
-	}
 	input1.value="";
-	var listItems=document.querySelectorAll(".checkbox.done");
+	closeButton.addEventListener("click",function(){
+	list.classList.add("delete");
+	todo.removeChild(list);
+	}
+	);//event listener on the x element along with function to removechild on click event
+	
+	const listItems = document.getElementsByClassName("task");
 	listArray.push(listItems);
 	taskListEmpty(); // call the function once here and at the end also
-
-}
-function deleteListElement() {
-	
-}
-
-
+}}
 buttonClick.addEventListener("click",createListElement);
 input1.addEventListener("keypress", function(event) {
 	if(event.key==="Enter") {createListElement();}
 }
 );
-
 taskListEmpty(); // call this function at the end so that it counts after all actions are taken
+
+  
+
 // end of checklist script
-
-// insert date function copied from ChatGPT and edited slightly
-const today = new Date();
-const options = { day: 'numeric' ,month: 'long'};
-const formattedDate = today.toLocaleDateString('en', options);
-document.getElementById("header").innerHTML="Tasks: "+formattedDate;
-
-
-
